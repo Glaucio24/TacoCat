1a94e364d1b6512dbed7397df9e27323794d4d80
Glaucio Alexandre <108555972+Glaucio24@users.noreply.github.com>
2023-04-05 19:29:04 -0500

    initial commit

--- js/site.js
+++ js/site.js
@@ -1,52 +1,45 @@
-//Get the string 
-//controller function
-function getSstring() {
-
-    // document.getElementById("alert").classList.add("invisible");
- 
-    let userString = document.getElementById("userInput").value;
-
-    let inputTag=document.getElementById("userInput");
-
-    let reverse = reverseString(userString);//declared a function into a variable 
-
-    
-
-    displayString(reverse);
-    submit(inputTag);
+function getValue(){
+    //make  sure alert is invisible
+    document.getElementById("alert").classList.add("invisible");
+    //get user string for the page 
+    let userString=document.getElementById("userInput").value;    
+    //check for a palidrome 
+    let palindrome=checkForPalindrome(userString);
+    //display out message to the screen
+    displayMessage(palindrome);
 }
+//check for palindrome
+function checkForPalindrome(userString){
 
-//Reverse the string 
-//logic function
-function reverseString(userString) {
-    let reverse = ''; //string or array to store reversed string  
+    userString=userString.toLowerCase();
 
-    for (let index = userString.length - 1; index >= 0; index--) {
-        reverse += userString[index];
+    //remove spaces and special caracters
+    let regex= /[^a-z0-9]/gi;
+    userString=userString.replace(regex,"");
+
+    //reverse string 
+    let revString=[];
+    let returnObj={};
+
+    //decrementing for loop 
+    for(let index=userString.length-1; index>=0; index--){
+        revString+=userString[index];
     }
-    return reverse;
+    if(revString==userString){
+        returnObj.msg="Well done! You entered a palindrome!";
+    }
+    else{
+        returnObj.msg="Sorry! you did not enter a palindrome!";
+    } 
+    returnObj.reversed=revString;
+
+    return returnObj;
 
 }
-//Submittion button
-function submit(inputTag){
-    console.log(inputTag);
-
-    inputTag.addEventListener("keyup", function(event){
-        if(event.keyCode === 13){
-            event.preventDefault();
-            displayString(reverseString(inputTag.value));
-        }       
-    });
-        
-
-}
-
-//Display the reversed string to the user//view function
-function displayString(reverse) {
-    console.log(reverse);
-
-    document.getElementById("msg").innerHTML = `Your string reversed is: ${reverse}`; //Template literals 
+//display a message to the string.
+function displayMessage(palindrome){
+    document.getElementById("alertHeader").innerHTML=palindrome.msg;
+    document.getElementById("msg").innerHTML=`Your phrase reversed is: ${palindrome.reversed}`;
     document.getElementById("alert").classList.remove("invisible");
-
+    
 }
-
